04c65a54b2c42c5a5ce8d1589e1042d4537d70c7
Grace Ilori <graceeilori@gmail.com>
2023-07-17 14:56:07 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char MyString[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", MyString);
+return (0);
+}
