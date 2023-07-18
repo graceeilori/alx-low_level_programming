c8a851b79dfa4826d2d847aab2ad366d4d7bd5c8
Grace Ilori <graceeilori@gmail.com>
2023-07-17 17:00:07 -0700

    Printing without printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * main - Entry point
- * write - Write content
+ * write - Writing the message to the standard error
  * Description: Printing
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 60);
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t length = strlen(message);
+write(2, message, length);
 return (1);
 }
+
